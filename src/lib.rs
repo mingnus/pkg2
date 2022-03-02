e3e39469378ed86390b5fac667ed8589d347133e
Ming-Hung Tsai <mingnus@gmail.com>
2022-03-02 16:21:33 +0800

    Add trait MetadataGenerator

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 pub fn foo() {
     pkg1::hello()
 }
+
+pub trait MetadataGenerator {
+    fn generate_metadata(&self, v: &dyn pkg1::MetadataVisitor);
+}
+
+//---
+
+pub struct CacheGenerator;
+
+impl MetadataGenerator for CacheGenerator {
+    fn generate_metadata(&self, v: &dyn pkg1::MetadataVisitor) {
+        println!("CacheGenerator.generate_metadata()");
+        v.superblock_b();
+    }
+}
